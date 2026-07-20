72f5e84046902076ba13755cfc36db4d46bdf5de
Ayansheikh47 <ayansheikh8767@gmail.com>
2026-07-20 16:17:12 +0530

    Reverse String Without using Built-In Functions

---
+++ Basic Problems/RevereseStringWUBF.java
@@ -0,0 +1,12 @@
+public class RevereseStringWUBF {
+    public static void main(String[] args) {
+        String name="Nitin";
+        String reverse="";
+
+        for (int i=name.length()-1; i>=0; i--){
+            reverse += name.charAt(i);
+        }
+        System.out.println(reverse);
+
+    }
+}
