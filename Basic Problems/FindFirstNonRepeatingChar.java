8c4f3825995cfdcc3158e02c1fd7c58af4ec9298
Ayansheikh47 <ayansheikh8767@gmail.com>
2026-07-20 16:15:04 +0530

    Find First Non Repeating Char From String

---
+++ Basic Problems/FindFirstNonRepeatingChar.java
@@ -0,0 +1,19 @@
+public class FindFirstNonRepeatingChar {
+    public static void main(String[] args) {
+        String s="recursive";
+
+        for (int i=0; i<s.length();i++){
+            int count=0;
+            for (int j=i+1; j<s.length(); j++){
+                if (s.charAt(i)==s.charAt(j)){
+                    count++;
+                }
+
+            }
+            if (count==0){
+                System.out.println("First non repeating char: "+s.charAt(i));
+                break;
+            }
+        }
+    }
+}
